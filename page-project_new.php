b3ead770225eb5e29517942c82b0a71ca1aecaad
Dmitry Yurkovsky <sobranieinfo@yandex.ru>
2023-02-11 23:15:01 +0700

    Version 0.3 on 11 feb 2023

--- page-project_new.php
+++ page-project_new.php
@@ -8,28 +8,29 @@
 </head>
 <body>
 
-<h1 class="title_1">New project</h1>
 
-<div class="container">
-    <header class="header row gy-3">
+
+<div class="out container-xl">
+    <h1 class="title_1">New project</h1>
+    <!--header class="header row gy-3">
         <div class="col-md-3">
             <div class="block">Logo</div>
         </div>
         <div class="col-md-9">
             <div class="block">Menu</div>
         </div>
-    </header>
+    </header-->
 
     <section class="promo row gy-3">
-        <div class="col-md-6">
+        <div class="col-lg-6">
             <div class="block">Text</div>
         </div>
-        <div class="col-md-6">
+        <div class="col-lg-6">
             <div class="block">Photo</div>
         </div>
     </section>
 
-    <section class="clients row gy-3">
+    <!--section class="clients row gy-3">
         <div class="col-sm-6 col-md-4 col-lg-2">
             <div class="block">Client</div>
         </div>
@@ -69,7 +70,7 @@
         <div class="col-sm-6 col-md-4 col-lg-2">
             <div class="block">Client</div>
         </div>
-    </section>
+    </section-->
 </div>
 </body>
 </html>
