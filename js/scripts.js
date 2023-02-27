827d41361bdc679e6b04e0c80dba467738362544
Dmitry Yurkovsky <sobranieinfo@yandex.ru>
2023-02-27 16:52:49 +0700

    Version 1.0 on 27 feb 2023 (footer and header)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 jQuery(function ($) {
     "use strict";
 // Custom jQuery Code Here
-
+    // to open mobile menu
+    $('.menu_burger').click (function(event){
+        $('.nav_mobile, .nav_mobile_close').toggleClass('active');
+    });
+    // to close mobile menu
+    $('.nav_mobile_close').click (function(event){
+        $('.nav_mobile, .nav_mobile_close').toggleClass('active');
+    });
 });
